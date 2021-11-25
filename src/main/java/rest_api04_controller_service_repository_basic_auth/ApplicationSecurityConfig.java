cacac3acd226a43053bd5de2bc9a311080639774
apple <apple@apples-imac-6.attlocal.net>
2021-11-24 20:02:10 -0500

    Third commit

--- src/main/java/rest_api04_controller_service_repository_basic_auth/ApplicationSecurityConfig.java
+++ src/main/java/rest_api04_controller_service_repository_basic_auth/ApplicationSecurityConfig.java
@@ -1,9 +1,16 @@
 package rest_api04_controller_service_repository_basic_auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -14,10 +21,32 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
 		
 		http.
 			authorizeRequests().
+			antMatchers("/", "index", "/css/*", "js/*").permitAll().
 			anyRequest().
 			authenticated().
 			and().
 			httpBasic();//Basic-Authentication: Uses username and password for every request
+	}
+	
+	private final PasswordEncoder passwordEncoder;	
+	@Autowired
+	public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+	}
+
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		
+		UserDetails student = User.
+								builder().
+								username("techproed").
+								password(passwordEncoder.encode("password123")).
+								roles("STUDENT").
+								build(); 
+		
+		return new InMemoryUserDetailsManager(student);
+
 	}
 	
 	
